28c5e674af5548a620902ebdfc3226d436a1d3e8
sansentest <ahlokywy@gmail.com>
2026-06-28 07:54:15 -0600

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
 import PropsPage from './pages/PropsPage';
 import HandlingPage from './pages/HandlingPage';
 import StatePage from './pages/StatePage';
